6cca8e95e2a13bd41bbf1dd56258b003732efe32
John Misoyianis <jmis@misoyianis.io>
2019-01-29 22:27:32 -0600

    got some scribbly scrabbly wibbly wobbly colors in a grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,50 @@
-function setup() {
+var canvasWidth = 640;
+var canvasHeight = 480;
+var bkgrndColor = [0,0,0];
+var gmBoard;
 
+function setup() {
+    gmBoard = new gameBoard();
+    createCanvas(canvasWidth,canvasHeight);
+    background(bkgrndColor);
+    gmBoard.randomize();
 }
 
 function draw() {
-    
+    gmBoard.draw();
+    gmBoard.randomize();
 }
+class gameBoard {
+    constructor(){
+        this.tiles = new Array(64);
+        for(var i=0;i<this.tiles.length;i++){
+            this.tiles[i] = new Array(48);
+        }
+    }
+    draw() {
+        for(var i=0;i<this.tiles.length;i++){
+            for(var j=0;j<this.tiles[i].length;j++){
+                fill(this.tiles[i][j].color);
+                ellipse(i*10+5,j*10+5,10,10);
+            }
+        }
+    }
+    randomize() {
+        for(var i=0;i<this.tiles.length;i++){
+            for(var j=0;j<this.tiles[i].length;j++){
+                this.tiles[i][j]=new square();
+            }
+        }
+    }
+}
+
+class square {
+    constructor(){
+        this.color = [random()*255,random()*255,random()*255]
+    }
+    draw() {
+
+    }
+}
+
+
