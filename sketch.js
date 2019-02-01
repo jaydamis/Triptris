d160e335ef7127f088817b5bfa34e36f95226417
John Misoyianis <jmis@misoyianis.io>
2019-01-31 20:00:40 -0600

    changed how piece location was handled to grid system

--- sketch.js
+++ sketch.js
@@ -15,15 +15,16 @@ function setup() {
 
 function draw() {
     keyCheck();
-    if(piecey.y > 24){
-        piecey = new piece();
-    }
     background(bkgrndColor);
-    //gmBoard.draw();
+    gmBoard.draw();
     gmBoard.randomize();
     piecey.draw();
     if(frame%10==0){
         piecey.update();
+        if(piecey.status == "REST"){
+            gmBoard.pieces.push(piecey);
+            piecey = new piece();
+        }
     }
     frame++;
     if(frame>30){
@@ -41,16 +42,20 @@ function keyCheck(){
 class gameBoard {
     constructor(){
         this.tiles = new Array(32);
+        this.pieces = new Array();
         for(var i=0;i<this.tiles.length;i++){
             this.tiles[i] = new Array(24);
         }
     }
     draw() {
-        for(var i=0;i<this.tiles.length;i++){
-            for(var j=0;j<this.tiles[i].length;j++){
-                fill(this.tiles[i][j].color);
-                ellipse(i*20+10,j*20+10,20,20);
-            }
+        // for(var i=0;i<this.tiles.length;i++){
+        //     for(var j=0;j<this.tiles[i].length;j++){
+        //         fill(this.tiles[i][j].color);
+        //         ellipse(i*20+10,j*20+10,20,20);
+        //     }
+        // }
+        for(var i=0;i<this.pieces.length;i++){
+            this.pieces[i].draw();
         }
     }
     randomize() {
@@ -64,76 +69,92 @@ class gameBoard {
 
 class piece {
     constructor() {
+        this.status = "PRIMARY";
+        var x=10;
+        var y=0;
+        this.color=[random()*255,random()*255,random()*255]
+        this.squares = new Array();
         var typeID = Math.floor(random(6));
-        if(typeID==0)
+        if(typeID==0){
             this.type = "square";
+            this.squares.push(new square(x,y,this.color));
+            this.squares.push(new square(x+1,y,this.color));
+            this.squares.push(new square(x,y+1,this.color));
+            this.squares.push(new square(x+1,y+1,this.color));
+        }
         else if(typeID==1){
             this.type = "elBlock";
+            this.squares.push(new square(x,y,this.color));
+            this.squares.push(new square(x+1,y,this.color));
+            this.squares.push(new square(x+1,y+1,this.color));
+            this.squares.push(new square(x+1,y+2,this.color));
         }
         else if(typeID==2){
             this.type = "reverseElBlock";
+            this.squares.push(new square(x+1,y,this.color));
+            this.squares.push(new square(x+1,y+1,this.color));
+            this.squares.push(new square(x+1,y+2,this.color));
+            this.squares.push(new square(x,y+2,this.color));
         }
         else if(typeID==3){
             this.type = "squiggly";
+            this.squares.push(new square(x,y,this.color));
+            this.squares.push(new square(x,y+1,this.color));
+            this.squares.push(new square(x+1,y+1,this.color));
+            this.squares.push(new square(x+1,y+2,this.color));
         }
         else if(typeID==4){
             this.type = "reverseSquiggly";
+            this.squares.push(new square(x+1,y,this.color));
+            this.squares.push(new square(x+1,y+1,this.color));
+            this.squares.push(new square(x,y+1,this.color));
+            this.squares.push(new square(x,y+2,this.color));
         }
-        else
+        else{
             this.type = "line";
-        this.color=[random()*255,random()*255,random()*255]
-        this.x=16;
-        this.y=1;
-    }
-    draw(){
-        fill(this.color);
-        if(this.type=="square"){
-            rect(this.x*20,this.y*20,20,20);
-            rect(this.x*20+20,this.y*20,20,20);
-            rect(this.x*20,this.y*20+20,20,20);
-            rect(this.x*20+20,this.y*20+20,20,20)
-        }
-        if(this.type=="line"){
             for(var i=0;i<4;i++){
-                rect(this.x*20,(this.y+i)*20,20,20);
+                this.squares.push(new square(x,y+i,this.color));                
             }
         }
-        if(this.type=="elBlock"){
-            rect(this.x*20,this.y*20,20,20);
-            rect((this.x+1)*20,this.y*20,20,20);
-            rect((this.x+1)*20,(this.y+1)*20,20,20);
-            rect((this.x+1)*20,(this.y+2)*20,20,20);        
-        }
-        if(this.type=="reverseElBlock"){
-            rect(this.x*20,this.y*20,20,20);
-            rect((this.x)*20,(this.y+1)*20,20,20);
-            rect((this.x)*20,(this.y+2)*20,20,20);
-            rect((this.x-1)*20,(this.y+2)*20,20,20);
-        }
-        if(this.type=="squiggly"){
-            rect((this.x)*20,(this.y)*20,20,20);
-            rect((this.x)*20,(this.y+1)*20,20,20);
-            rect((this.x+1)*20,(this.y+1)*20,20,20);
-            rect((this.x+1)*20,(this.y+2)*20,20,20);            
-        }
-        if(this.type=="reverseSquiggly"){
-            rect((this.x)*20,(this.y)*20,20,20);
-            rect((this.x)*20,(this.y+1)*20,20,20); 
-            rect((this.x-1)*20,(this.y+1)*20,20,20); 
-            rect((this.x-1)*20,(this.y+2)*20,20,20);          
+
+    }
+    draw(){
+        for(var i=0;i<this.squares.length;i++){
+            this.squares[i].draw();
         }
     }
     update(){
-        this.y++;
+        if(this.checkCollision("DOWN")!=true){
+            for(var i=0;i<this.squares.length;i++){
+                this.squares[i].drop();
+            }
+        }
+    }
+    checkCollision(direction){
+        if(direction=="DOWN"){
+            for(var i=0;i<this.squares.length;i++){
+                if(this.squares[i].y==23){
+                    this.status = "REST"
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
 
 class square {
-    constructor(){
-        this.color = [random()*255,random()*255,random()*255]
+    constructor(x,y,color){
+        this.color = color;
+        this.x = x;
+        this.y = y;
     }
     draw() {
-
+        fill(this.color);
+        rect(this.x*20,this.y*20,20,20);
+    }
+    drop(){
+        this.y++;
     }
 }
 
